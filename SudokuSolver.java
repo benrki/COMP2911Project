2864c8ac6d498aa8d31fd1b4226fa04bc3db7caa
nathanho <nathandavisho@gmail.com>
2013-05-19 23:18:55 +1000

    Create SudokuSolver.java

---
+++ SudokuSolver.java
@@ -0,0 +1,18 @@
+public class SudokuSolver {
+
+    // (Make blank grid)
+    // Input External Puzzle
+    // (Every input has given==true)
+    // Play
+    // (Checks if hasSolution, returns grid)
+    // 
+    
+    
+    
+    
+    
+    // void startTime()
+    // void getTime()
+    // void undo()
+    // void redo()
+}
