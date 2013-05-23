747e99c7e357a343b5ec6bcbf9e313db0154170e
Nathan Ho <nathandavisho@gmail.com>
2013-05-24 01:43:17 +1000

    Fixed isGridCorrect bug

--- Model/SudokuModelTest.java
+++ Model/SudokuModelTest.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class SudokuModelTest {
     public static void main(String[] args) {
-        
+        /*
         // Testing generating puzzles
         SudokuModel sudoku = new SudokuModel();
         //sudoku.generatePuzzle();
@@ -37,7 +37,7 @@ public class SudokuModelTest {
         System.out.println("Undo (should remove 7)");
         sudoku.undoMove();
         sudoku.printCurrentGrid();      
-        
+        */
         
         // Testing generating puzzles
         /*SudokuModel sudokuSolver = new SudokuModel();
@@ -54,8 +54,19 @@ public class SudokuModelTest {
         sudokuSolver.printAnswerGrid();
         sudokuSolver.printCurrentGrid();
         */
+        
         // Testing findSolution, setting and solve.
-        /*
+        String input = 
+                "0 5 9 1 1 1 1 1 8\n" +
+                "7 0 2 0 5 0 0 4 1\n" + 
+                "0 0 0 3 2 8 0 0 0\n" +
+                "0 4 0 1 0 0 7 6 5\n" +
+                "0 0 5 7 0 2 8 0 0\n" +
+                "9 7 3 0 0 5 0 1 0\n" +
+                "0 0 4 2 6 7 0 0 0\n" +
+                "8 3 0 0 1 0 2 0 4\n" +
+                "1 0 0 8 0 0 5 9 0\n";
+        
         String solution = 
                 "3 5 9 4 7 1 6 2 8\n" +
                 "7 8 2 9 5 6 3 4 1\n" +
@@ -67,37 +78,38 @@ public class SudokuModelTest {
                 "8 3 6 5 1 9 2 7 4\n" + 
                 "1 2 7 8 3 4 5 9 6\n";
         
-        SudokuSolver sudokuSolver = new SudokuSolver();
+        SudokuModel sudoku = new SudokuModel();
         // Should print all blank first
-        sudokuSolver.printCurrentGrid();
+        sudoku.printCurrentGrid();
         // Read input
         Scanner s = new Scanner(input);
         for(int i=0; i<Grid.NUM_ROWS; i++) {
             for(int j=0; j<Grid.NUM_COLS; j++) {
                 int n = s.nextInt();
                 if(n!=0) {
-                    sudokuSolver.giveCellNumber(i, j, n);
+                    sudoku.giveCellNumber(i, j, n);
                 }
             }
         }
         s.close();
-        sudokuSolver.findSolution();
+        sudoku.findSolution();
                 
         System.out.println("Unsolved grid");
-        sudokuSolver.printCurrentGrid();
-        
-        System.out.println("Putting some numbers in");
-        sudokuSolver.setCellNumber(0, 0, 3);
-        sudokuSolver.setCellNumber(8, 1, 2);
-        sudokuSolver.setCellNumber(4, 4, 4);
-        sudokuSolver.printCurrentGrid();
+        sudoku.printCurrentGrid();
+        sudoku.printAnswerGrid();
+        /*System.out.println("Putting some numbers in");
+        sudoku.setCellNumber(0, 0, 3);
+        sudoku.setCellNumber(8, 1, 2);
+        sudoku.setCellNumber(4, 4, 4);
+        sudoku.printCurrentGrid();
         
         System.out.println("Trying to put a number in a cell that was given");
-        sudokuSolver.setCellNumber(0, 1, 2);
+        sudoku.setCellNumber(0, 1, 2);
         
         System.out.println("Reveal solution");
-        sudokuSolver.revealSolution();
-        sudokuSolver.printCurrentGrid();
+        sudoku.revealSolution();
+        sudoku.printCurrentGrid();
+        sudoku.printAnswerGrid();
 
         
         System.out.println("Compare to (hard-coded) solution");
