2d2fb19c737fd7fd0776b99c3aba3830b9d64db1
Benjamin Ki <benjaminrki@gmail.com>
2013-05-23 21:58:05 +1000

    position

---
+++ Viewer/Position.java
@@ -0,0 +1,34 @@
+
+/**
+ * @author Ben
+ *
+ */
+public class Position {
+	private int x;
+	private int y;
+	
+	/**
+	 * @param x
+	 * @param y
+	 */
+	public Position(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * @return
+	 */
+	public int getX() {
+		return x;
+	}
+
+	/**
+	 * @return
+	 */
+	public int getY() {
+		return y;
+	}
+	
+	
+}
