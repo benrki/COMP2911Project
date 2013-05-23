0922aca2e236020851930e57f32df6607e8c8439
Benjamin Ki <benjaminrki@gmail.com>
2013-05-23 19:21:14 +1000

    Menubar work

--- GUI/MainInterface.java
+++ GUI/MainInterface.java
@@ -3,6 +3,7 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
 import javax.swing.ImageIcon;
@@ -10,7 +11,9 @@ import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.KeyStroke;
 
 
@@ -21,7 +24,8 @@ import javax.swing.KeyStroke;
  *
  */
 public class MainInterface {
-
+	private static final String ABOUT_MESSAGE = "2013s1 COMP2911 Project\n\nCreated by Benjamin Ki, Kenny Ho, Nathan Ho and Denaysh Selvakkumar.";
+	
 	public static void main(String[] Args) {
 		JFrame mainFrame = new JFrame("Sudoku");
 		
@@ -55,7 +59,7 @@ public class MainInterface {
 
 	private static JMenuBar createMenuBar() {
 		JMenuBar menuBar = new JMenuBar();
-		JMenu newGameMenu = new JMenu("New Game (N)");
+		JMenu newGameMenu = new JMenu("New Game");
 		newGameMenu.setMnemonic(KeyEvent.VK_N);
 		newGameMenu.getAccessibleContext().setAccessibleDescription(
 				"Create a new game");
@@ -64,20 +68,35 @@ public class MainInterface {
 		JMenuItem easy = new JMenuItem("Easy");
 		easy.setAccelerator(KeyStroke.getKeyStroke(
 		        KeyEvent.VK_1, ActionEvent.ALT_MASK));
+		easy.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				JOptionPane.showMessageDialog(null, "Filler option item");
+			}
+		});
 		newGameMenu.add(easy);
 		
 		JMenuItem medium = new JMenuItem("Medium");
 		medium.setAccelerator(KeyStroke.getKeyStroke(
 		        KeyEvent.VK_2, ActionEvent.ALT_MASK));
+		medium.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				JOptionPane.showMessageDialog(null, "Filler option item");
+			}
+		});
 		newGameMenu.add(medium);
 		
 		JMenuItem hard = new JMenuItem("Hard");
 		hard.setAccelerator(KeyStroke.getKeyStroke(
 		        KeyEvent.VK_3, ActionEvent.ALT_MASK));
+		hard.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				JOptionPane.showMessageDialog(null, "Filler option item");
+			}
+		});
 		newGameMenu.add(hard);
 		
 		JMenu optionsMenu = new JMenu("Settings");
-		optionsMenu.setMnemonic(KeyEvent.VK_O);
+		optionsMenu.setMnemonic(KeyEvent.VK_S);
 		optionsMenu.getAccessibleContext().setAccessibleDescription(
 				"Application settings");
 		menuBar.add(optionsMenu);
@@ -85,6 +104,11 @@ public class MainInterface {
 		JMenuItem temp = new JMenuItem("Filler");
 		temp.setAccelerator(KeyStroke.getKeyStroke(
 		        KeyEvent.VK_1, ActionEvent.ALT_MASK));
+		temp.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				JOptionPane.showMessageDialog(null, "Filler option item");
+			}
+		});
 		optionsMenu.add(temp);
 		
 		JMenu aboutMenu = new JMenu("About");
@@ -96,10 +120,13 @@ public class MainInterface {
 		JMenuItem aboutItem = new JMenuItem("About Sudoku");
 		aboutItem.setAccelerator(KeyStroke.getKeyStroke(
 		        KeyEvent.VK_1, ActionEvent.ALT_MASK));
+		aboutItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				JOptionPane.showMessageDialog(null, ABOUT_MESSAGE);
+			}
+		});
 		aboutMenu.add(aboutItem);
-		
 		return menuBar;
 	}
-	
 	
 }
