8054dcde9f5e2428740db48ad2b1b2f59db52ea2
nathanho <nathandavisho@gmail.com>
2013-06-01 20:24:52 +1100

    Update ModelEasyGenerator.java

--- ModelEasyGenerator.java
+++ ModelEasyGenerator.java
@@ -1,13 +1,13 @@
-public class EasyGenerator extends SudokuGenerator{
+public class ModelEasyGenerator extends ModelGenerator{
     // The number of "given" cells.
 	private static final int EASY_DIFFICULTY = 60;
-	public EasyGenerator(Grid currentGrid, Grid answerGrid) {
+	public ModelEasyGenerator(ModelGrid currentGrid, ModelGrid answerGrid) {
 		super(currentGrid, answerGrid);
 	}
 	
 	@Override
 	protected void removeCells() {
-        int remove = Grid.NUM_CELLS - EASY_DIFFICULTY;
+        int remove = ModelGrid.NUM_CELLS - EASY_DIFFICULTY;
         for(int i=0; i<remove; i++) {
             this.removeRandomCellInBox(i%9);
         }
