90b6306fd434d9009816823829544a60d05c444f
nathanho <nathandavisho@gmail.com>
2013-06-01 20:26:11 +1100

    Update ModelHardGenerator.java

--- ModelHardGenerator.java
+++ ModelHardGenerator.java
@@ -1,14 +1,14 @@
-public class HardGenerator extends SudokuGenerator{
+public class ModelHardGenerator extends ModelGenerator{
     // The number of "given" cells.
     private static final int HARD_DIFFICULTY = 30;
 
-    public HardGenerator(Grid currentGrid, Grid answerGrid) {
+    public ModelHardGenerator(ModelGrid currentGrid, ModelGrid answerGrid) {
         super(currentGrid, answerGrid);
-    }
+	}
 	
     @Override
     protected void removeCells() {
-        int remove = Grid.NUM_CELLS - HARD_DIFFICULTY;
+        int remove = ModelGrid.NUM_CELLS - HARD_DIFFICULTY;
         for(int i=0; i<remove; i++) {
             this.removeRandomCellInBox(i%9);
         }
